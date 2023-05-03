91c3b31a5135099274584728d96b5928ebcfff4c
Ayat Rahman <57280365+ayat1041@users.noreply.github.com>
2023-05-03 13:42:29 +0600

    loaded data at /chef

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-
+const chef = require('./data/chef.json');
 app.get('/',(req,res)=>{
     res.send('desi_eats_server_green');
+});
+
+app.get('/chef',(req,res)=>{
+    res.send(chef);
 });
 
 app.listen(port,()=>{
