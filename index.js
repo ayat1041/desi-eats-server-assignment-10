a0b79087fa6c8cb194ca3e962c36ae6ded7c75d8
Ayat Rahman <57280365+ayat1041@users.noreply.github.com>
2023-05-03 13:44:26 +0600

    setup cors - done

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const chef = require('./data/chef.json');
+const cors = require('cors');
+
+app.use(cors());
+
 app.get('/',(req,res)=>{
     res.send('desi_eats_server_green');
 });
