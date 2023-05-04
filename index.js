16da0137f160a5890d25f0c765297eed8bf172ef
Ayat Rahman <57280365+ayat1041@users.noreply.github.com>
2023-05-04 20:16:25 +0600

    server ready to deploy

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const chef = require('./data/chef.json');
+const tips = require('./data/tips.json');
 const cors = require('cors');
 
 app.use(cors());
@@ -12,6 +13,14 @@ app.get('/',(req,res)=>{
 
 app.get('/chef',(req,res)=>{
     res.send(chef);
+});
+app.get('/tips',(req,res)=>{
+    res.send(tips);
+});
+app.get('/chef/:id',(req,res)=>{
+    const id = req.params.id
+    const data = chef.chefs.find(c => c.id == id);
+    res.send(data);
 });
 
 app.listen(port,()=>{
